8a845d63aae89d282e4124ae9ad94b09c97a1f1d
phunq71 <ngoquocphu71@gmail.com>
2026-02-23 16:12:37 +0700

    temp

--- Home/ayanavita-fontend-final/src/api/http.ts
+++ Home/ayanavita-fontend-final/src/api/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
+const baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:8090";
 
 export const http = axios.create({
   baseURL,
